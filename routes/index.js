e3e17788376305c677897367f5701f82c41b7b29
airrakeshkumarsharma <airrakeshkumarsharma@gmail.com>
2019-01-11 08:55:31 +0530

    ok

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var monk = require('monk');  //get the monk javascript
 var moment = require('moment'); //get the moment data
 var dbs = monk('localhost:27017/3dclikx'); //connect the data base
-var login = dbs.get('admin'); // Login by admin collection
+var login = dbs.get('user'); // user login/signup collection
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
