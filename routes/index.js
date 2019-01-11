0df2e48951bb18101015f21800ae531d15f6c370
airrakeshkumarsharma <airrakeshkumarsharma@gmail.com>
2019-01-11 08:17:07 +0530

    monk and momnet package is added to access the connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var monk = require('monk');  //get the monk javascript
+var moment = require('moment'); //get the moment data
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 router.get('/login',function(req, res, next){
 	res.render('login');
